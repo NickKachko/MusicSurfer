04236b0a99edd9c5c00f2692d663c05b72bd6661
n.kachko <justmynk@gmail.com>
2015-11-28 15:24:47 +0200

    Created a frame of server
    Listens to the connections and prints what its recieves

---
+++ Server/ConnectionListener.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class ConnectionListener
+    {
+        private TcpListener server;
+        private Int32 serverPort;
+        private IPAddress serverAddress;
+        private Byte[] bytes, response;
+        private String data;
+        private Int32 maxConnections;
+        private Thread listeningThread;
+
+
+        private void InitNet()
+        {
+            serverPort = 3737;
+            server = new TcpListener(serverAddress, serverPort);
+            server.Start();
+            bytes = new Byte[256];
+            data = null;
+        }
+
+        private IPAddress GetServerIp()
+        {
+            IPHostEntry host;
+            IPAddress localIP = null;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip;
+                }
+            }
+            return localIP;
+        }
+
+        private void AcceptConnection()
+        {
+            while (true)
+            {
+                Console.WriteLine("Waiting for a connection... ");
+                TcpClient client = server.AcceptTcpClient();
+                IPEndPoint remoteIpEndPoint = client.Client.RemoteEndPoint as IPEndPoint;
+                Console.WriteLine("Connected a client with IP " + remoteIpEndPoint.Address + " on port number " + remoteIpEndPoint.Port);
+                NetworkStream stream = client.GetStream();
+
+                Console.WriteLine();
+
+                int i = stream.Read(bytes, 0, bytes.Length);
+                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                Console.WriteLine("Received: {0}", data);
+
+                client.Close();
+            }
+        }
+
+        public ConnectionListener(int _maxConnections = 10)
+        {
+            serverAddress = GetServerIp();
+            InitNet();
+            listeningThread = new Thread(new ThreadStart(AcceptConnection));
+            maxConnections = _maxConnections;
+        }
+
+        public void Run()
+        {
+            listeningThread.Start();
+            Console.WriteLine("Server started with IP " + serverAddress.ToString());
+        }
+
+        public void Stop()
+        {
+            listeningThread.Abort();
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using VkNet;
 using VkNet.Enums.Filters;
@@ -19,22 +20,25 @@ namespace Server
             string password = "";
             Settings settings = Settings.All;
 
-            var api = new VkApi();
-            api.Authorize(appId, email, password, settings);
+            //var api = new VkApi();
+            //api.Authorize(appId, email, password, settings);
 
-            var music = api.Audio.Get((long)api.UserId, count: 10);
+            //var music = api.Audio.Get((long)api.UserId, count: 10);
             
             
-            foreach (var audio in music)
-            {
-                Console.WriteLine(audio.Id + "\t" + audio.Artist + "\t" + audio.Title);
-            }
-            Console.WriteLine("Downloading...");
-            using (var client = new WebClient())
-            {
-                client.DownloadFile(music[audioId].Url, music[audioId].Artist + " - " + music[audioId].Title + ".mp3");
-            }
-            Console.WriteLine("Seems to be downloaded.");
+            //foreach (var audio in music)
+            //{
+            //    Console.WriteLine(audio.Id + "\t" + audio.Artist + "\t" + audio.Title);
+            //}
+            //Console.WriteLine("Downloading...");
+            //using (var client = new WebClient())
+            //{
+            //    client.DownloadFile(music[audioId].Url, music[audioId].Artist + " - " + music[audioId].Title + ".mp3");
+            //}
+            //Console.WriteLine("Seems to be downloaded.");
+
+            ConnectionListener connectionListener = new ConnectionListener();
+            connectionListener.Run();
             Console.ReadKey();
         }
     }
